032a0e69e56b90cda854526dadcb90ffbd115b71
Simon Angoluan <sca1@williams.edu>
2023-05-07 22:32:08 -0400

    Final changes

--- ethics.py
+++ ethics.py
@@ -19,7 +19,16 @@ distinguish the chatbot responses from those of a human?
 
 Q1_your_answer = """
 
-Delete and put your answer here
+It is possible for users of our chatbot to associate human characteristics to it.
+This is because we have designed the chatbot's responses to user input to mimic human
+responses, and sympathize with the user. Therefore, users could potentially attribute human
+characteristics to our chatbot. One ramification of this is that the user
+could develop unrealistic expectations and emotions towards our chatbot, which can lead to
+frustration and disappointment when the chatbot is unable to meet the user's expectations.
+To make it easier for a user to easily distinguish the chatbot responses from those of a human,
+the chatbot could include disclaimers to notify the users that it is a model (like ChatGPT does),
+and use different fonts, colors, or formatting for the chatbot responses to make
+them distinct from the user's messages.
 
 """
 
@@ -35,13 +44,14 @@ Can you think of ways that designers of the chatbot can help to mitigate this ri
 
 Q2_your_answer = """
 
-Our chatbot does not currently store information about the user such as their preferences, 
-name, and instances of prior interaction with out bot. This greatly reduces the risk of 
-leaking private information. However, a number of modern chatbots store information about 
-users on their servers, so that they can provide personalized content. However any such data 
-must be collected with explicit approval from the user, and attempts should be made from 
-encouraging users to implement strong passwords, to using secure networking, and storage 
-techniques.  
+Our chatbot does not currently store information about the user such as their preferences,
+personal information, and data about prior user interaction with our bot.
+Because our chatbot does not store any personal information identified with a specific user,
+the possibility of a data breach is low. However, a number of modern chatbots do store information about users
+on their servers, so that they can provide personalized content. Any private user data must be
+collected with explicit approval from the user, and steps should be undertaken to
+implement strong data encryption and security protocols to protect user data. Moreover
+Users should have complete control over their data, and should be able to delete or modify it at any time.
 
 """
 
@@ -59,7 +69,17 @@ citizens -- should do in anticipation of these risks and/or in response to these
 
 Q3_your_answer = """
 
-Delete and put your answer here
+Advances made in modern dialogue systems have huge social costs and benefits.
+However, different stakeholders such as employees of the companies making these products or
+citizens do not have much input into creating these products.
+The first step must be making sure that these stakeholders have a seat in the
+descision making process about how these tools are created and deployed.
+There should also be a way to redistribute some of the gains (and profits) from the
+deployment of these technologies to the citizens so that the social costs of massive
+labour displacement in customer-service, translation and programming industry can be mitigated.
+Elected government officials should make sure that that these technologies are not
+deployed in a way that threatens social order and provide training or re-skilling
+programs for workers whose jobs are affected by automation.
 
 """
 
@@ -73,6 +93,12 @@ compared to an end-to-end deep learning approach, e.g. ChatGPT?
 
 Q4_your_answer = """
 
-Delete and put your answer here
+One advantage of building a frame-based model such as ours is the modularity and ability
+to use different machine learning techniques for specific tasks. However one limitation of this model
+compared to a chat-ChatGPT type model is that it is not very genralizable or flexible and
+can not properly handle input/tasks that it has not been explicitly programmed for.
+Building these modular pieces is also very time-consuming since a different technique
+is used for each task, compared to a bigger neural network model where a uniform bigger base
+model is used to mini-tasks. 
 
 """
